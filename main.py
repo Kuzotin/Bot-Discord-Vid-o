7504fb4b5670a47b445f5e342eca6267823af760
Kuzotin <88931811+Kuzotin@users.noreply.github.com>
2021-08-23 19:17:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -101,21 +101,4 @@ async def unmute(ctx, member : discord.Member, *, reason = "Aucune raison n'a é
     await member.remove_roles(mutedRole, reason = reason)
     await ctx.send(f"{member.mention} a été unmute !")
 
-status = ["//help",
-		"A votre service",
-		"L'eau mouille", 
-		"Le feu brule", 
-		"Lorsque vous volez, vous ne touchez pas le sol", 
-		"Winter is coming", 
-		"Mon créateur est Kuzotin", 
-		"Il n'est pas possible d'aller dans l'espace en restant sur terre", 
-		"La terre est ronde",
-		"La moitié de 2 est 1",
-		"7 est un nombre heureux",
-		"Les allemands viennent d'allemagne",
-		"Le coronavirus est un virus se répandant en Europe, en avez vous entendu parler ?",
-		"J'apparais 2 fois dans l'année, a la fin du matin et au début de la nuit, qui suis-je ?",
-		"Le plus grand complot de l'humanité est",
-		"Pourquoi lisez vous ca ?"]
-
 bot.run("T'as cru que j'allais te donner le token")
